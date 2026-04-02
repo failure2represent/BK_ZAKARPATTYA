4d6604d9ffb43cc8e759490c55094052c5b9c92d
failure2represent <markvalonski@gmail.com>
2026-04-02 06:58:23 +0200

    without swiper2

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 1,
-  spaceBetween: 60,
   autoplay: {
     delay: 5000,
+    speed: 1011,
   },
-  speed: 700,
+  speed: 1011,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -14,3 +14,12 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+// const swiperС = new Swiper('.swiperС', {
+//   loop: true,
+//   slidesPerView: 3,
+//   speed: 1011,
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+
+// });
