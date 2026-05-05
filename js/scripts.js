fad87d7b37c82f1f9ef3deb47f60e06ed92aa717
failure2represent <markvalonski@gmail.com>
2026-05-05 12:48:20 +0200

    mnr_dsgn_upd

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,34 @@ const swiper = new Swiper('.swiperGallery', {
 });
 
 const swiperС = new Swiper('.swiperCertificates', {
-  slidesPerView: 3,
+  slidesPerView: 4,
   spaceBetween: 60,
   touchEventsTarget: "container",
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
+});
+
+const logo = document.querySelector('.header__logo');
+
+logo?.addEventListener('click', function (e) {
+  const isHomePage =
+    window.location.pathname.endsWith('/index.html') ||
+    window.location.pathname.endsWith('/');
+
+  if (isHomePage) {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+
+    history.replaceState(
+      null,
+      '',
+      window.location.pathname + window.location.search
+    );
+  }
 });
